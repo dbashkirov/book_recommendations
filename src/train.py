91ca49ac9a421856ae97a319834b4dbee450bbbb
dbashkirov <bashkirovdp@gmail.com>
2023-05-14 00:20:00 +0000

    stop tracking processed

--- src/train.py
+++ src/train.py
@@ -1,15 +1,36 @@
+import click
 from utils import prepare_ratings
 from model import Model
 import scipy.sparse as sp
 
 
-ratings = prepare_ratings()
-print("Ratings prepared")
+@click.command()
+@click.argument(
+    "book_features_path", type=click.Path(), default="models/book_features.npz"
+)
+@click.argument(
+    "old_ratings_path", type=click.Path(), default="data/raw/old_ratings.csv"
+)
+@click.argument("new_ratings_path", type=click.Path(), default="data/raw/ratings.csv")
+@click.argument("n_epochs", type=click.INT, default=10)
+def train(
+    book_features_path: str = "models/book_features.npz",
+    old_ratings_path: str = "data/raw/old_ratings.csv",
+    new_ratings_path: str = "data/ratings.csv",
+    n_epochs: int = 10,
+):
+    ratings = prepare_ratings(old_ratings_path, new_ratings_path)
+    click.echo("Ratings prepared")
 
-book_features = sp.load_npz("models/book_features.npz")
-print("Book features loaded")
+    book_features = sp.load_npz(book_features_path)
+    click.echo("Book features loaded")
 
-model = Model()
-print("Model initialized")
+    model = Model()
+    click.echo("Model initialized")
 
-model.fit(ratings, book_features)
+    model.fit(ratings, book_features, n_epochs=n_epochs)
+    click.echo("Model fitted")
+
+
+if __name__ == "__main__":
+    train()
